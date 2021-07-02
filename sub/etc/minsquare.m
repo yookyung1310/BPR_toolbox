309099e8eb24cd0dc43ba866d8868ebac34ed915
yookyung1310 <yookyung1310@gmail.com>
2021-07-02 23:09:34 +0900

    Delete sub directory

--- sub/etc/minsquare.m
+++
@@ -1,7 +0,0 @@
-function [nRow, nCol] = minsquare(nElement)
-% find the minimum square (nRow x nMin) larger than nElemnt
-nCol = ceil(sqrt(nElement));
-nRow = ceil(nElement/ceil(sqrt(nElement)));
-
-
-
--- sub/peaknorm.m
+++
@@ -1,6 +0,0 @@
-function yNorm = peaknorm(y)
-% peak normalize event locked response
-% y = T x N
-yNorm = y./abs(min(y)); 
-end
-
